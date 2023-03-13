d6df300f82a86075afe0f8d3e94c71814042a4b6
Usshel <pawelopadczuk@gmail.com>
2023-03-13 17:33:21 +0100

    feat:Common Module added

--- src/app/components/auth-login/auth-login.component-module.ts
+++ src/app/components/auth-login/auth-login.component-module.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthLoginComponent } from './auth-login.component';
 
 @NgModule({
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   declarations: [AuthLoginComponent],
   providers: [],
   exports: [AuthLoginComponent]
