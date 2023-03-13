1465f6d572ef3c24b460cdd50d644f84275fda40
Usshel <pawelopadczuk@gmail.com>
2023-03-13 21:23:41 +0100

    feat: imports added

--- src/app/components/auth-register/auth-register.component-module.ts
+++ src/app/components/auth-register/auth-register.component-module.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRegisterComponent } from './auth-register.component';
 
 @NgModule({
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   declarations: [AuthRegisterComponent],
   providers: [],
   exports: [AuthRegisterComponent]
