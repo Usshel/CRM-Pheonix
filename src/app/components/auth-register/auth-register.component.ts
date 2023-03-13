1db09edc4c42ad4dbbe8b2e55435042404d6292f
Usshel <pawelopadczuk@gmail.com>
2023-03-13 21:22:40 +0100

    feat: register form created feat : validators added

--- src/app/components/auth-register/auth-register.component.ts
+++ src/app/components/auth-register/auth-register.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {FormControl, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../services/validators.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-auth-register',
@@ -7,4 +10,27 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AuthRegisterComponent {
+  readonly register: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      this._validatorsService.hasACapitalLetter,
+      this._validatorsService.hasASmallLetter,
+      Validators.minLength(8),
+      this._validatorsService.hasANumber,
+      this._validatorsService.hasASpecialSign,
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+    ])
+  },
+    [this._validatorsService.samePassword]
+  );
+
+  constructor(private _validatorsService: ValidatorsService, private _authService: AuthService) {
+  }
+
+  onRegisterSubmitted(register: FormGroup): void {
+
+  }
 }
