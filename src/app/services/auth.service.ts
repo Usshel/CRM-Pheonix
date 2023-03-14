04fb3d5dcb2b46a2bec30d671ea6e3f0ca0c3cfe
Usshel <pawelopadczuk@gmail.com>
2023-03-14 20:19:44 +0100

    feat: login server response mapped feat: Subject for accessToken created feat: Subject for refreshToken created

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private _currentUserAccessTokenSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  public currentUserAccessToken$: Observable<string> = this._currentUserAccessTokenSubject.asObservable();
+
+  private _currentUserRefreshTokenSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  public currentUserRefreshToken$: Observable<string> = this._currentUserRefreshTokenSubject.asObservable();
+
+  private _currentUserEmailVerifiedSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  public currentUserEmailVerified$: Observable<string> = this._currentUserEmailVerifiedSubject.asObservable();
+
   constructor(private _httpClient: HttpClient) {
   }
 
+
+
+
+
+
   login(userEmail: string, userPassword: string): Observable<any> {
-    return this._httpClient.post('https://us-central1-courses-auth.cloudfunctions.net/auth/login', { data: { email: userEmail, password: userPassword } }, undefined).pipe(
-      tap((data) => console.log(data)
-    ));
+    return this._httpClient.post<any>('https://us-central1-courses-auth.cloudfunctions.net/auth/login',
+      { data: { email: userEmail, password: userPassword } }, undefined)
+      .pipe(
+        map((credentials) => ({
+          accessToken: credentials.data.accessToken,
+          refreshToken: credentials.data.refreshToken,
+        })),
+        tap((credentials) => console.log(credentials)
+        ));
   }
 
   register(userEmail: string, userPassword: string): Observable<any> {
-    return this._httpClient.post<any>('https://us-central1-courses-auth.cloudfunctions.net/auth/register2', {data: { email: userEmail, password: userPassword}}, undefined).pipe(
-      tap((data) => console.log(data)
-    ))
+    return this._httpClient.post<any>('https://us-central1-courses-auth.cloudfunctions.net/auth/register2',
+      { data: { email: userEmail, password: userPassword } }, undefined)
+      .pipe(
+        tap((data) => console.log(data)
+        ))
   }
 };
