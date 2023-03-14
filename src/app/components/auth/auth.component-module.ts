39da31cb4f12b1604500eaf504030c1ff1f7973c
Usshel <pawelopadczuk@gmail.com>
2023-03-14 21:56:29 +0100

    feat: Auth component created

---
+++ src/app/components/auth/auth.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth.component';
+
+@NgModule({
+  imports: [],
+  declarations: [AuthComponent],
+  providers: [],
+  exports: [AuthComponent]
+})
+export class AuthComponentModule {
+}
